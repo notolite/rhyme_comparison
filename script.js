43197fbb1e0bf48b9c4af0d2ef154786c19ec9a6
notolyte <mina37s.sumomo@gmail.com>
2023-08-13 18:53:33 +0900

    メンテナンス容易化

--- script.js
+++ script.js
@@ -6,24 +6,19 @@ window.onload = () => {
     hideMain();
 }
 function hideMain() {
-    const putrow = Array.from(document.getElementsByClassName("普通話"));
-    const guarow = Array.from(document.getElementsByClassName("広州話"));
-    const korrow = Array.from(document.getElementsByClassName("朝鮮語"));
-    const jparow = Array.from(document.getElementsByClassName("漢音"));
-    const jgorow = Array.from(document.getElementsByClassName("呉音"));
     const rows = document.getElementsByTagName("table")[0].rows;
     let hide = dialectnum;
     setRowSpan(rows, hide);
-    const 官話 = document.forms.hide.h_row[0].checked;
-    const 粤語 = document.forms.hide.h_row[1].checked;
-    const 韓語 = document.forms.hide.h_row[2].checked;
-    const 漢音 = document.forms.hide.h_row[3].checked;
-    const 呉音 = document.forms.hide.h_row[4].checked;
-    if (!官話) { addHideAttribute(putrow); hide--; } else { removeHideAttribute(putrow); }
-    if (!粤語) { addHideAttribute(guarow); hide--; } else { removeHideAttribute(guarow); }
-    if (!韓語) { addHideAttribute(korrow); hide--; } else { removeHideAttribute(korrow); }
-    if (!漢音) { addHideAttribute(jparow); hide--; } else { removeHideAttribute(jparow); }
-    if (!呉音) { addHideAttribute(jgorow); hide--; } else { removeHideAttribute(jgorow); }
+    for (let i = 0; i < dialectnum - 2; i++) {
+        const targetrows = Array.from(document.getElementsByClassName(document.forms.hide.h_row[i].value));
+        console.log(targetrows);
+        if (document.forms.hide.h_row[i].checked) {
+            removeHideAttribute(targetrows);
+        } else {
+            addHideAttribute(targetrows);
+            hide--;
+        }
+    }
     setRowSpan(rows, hide);
 }
 
